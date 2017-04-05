d73ef509208c8a7f22e6a4bc7dc85a6a5f6abfb5
Laura Logan <laurardh@aol.com>
2017-04-05 12:50:30 -0400

    adds enemy, player(player not rendering at moment)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 5;
+    this.y = 225;
+    this.speed = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if(this.x > 500) {
+     this.x = -50;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,41 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-princess-girl.png';
+  this.x = 300;
+  this.y = 300;
+};
 
+Player.prototype.handleInput = function(direction) {
+    if (direction === 'up') {
+        this.x += 100;
+    }
+    if (direction === 'down') {
+        this.x -= 100;
+    }
+    if (direction === 'right') {
+        this.y += 100 ;
+    }
+    if (direction === 'left') {
+        this.y -= 100;
+    }
+};
 
+Player.prototype.update = function(dt){
+
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy()];
+allEnemies.push(new Enemy(10, 50));
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
